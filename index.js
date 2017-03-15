cc09664d7271d9a63bdb28c817c3492162dedc95
Aleksandr Strutynskyy <alekzzz@gmail.com>
2017-03-14 22:56:52 -0700

    Fixed module paths

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-process.env.NODE_PATH = __dirname;
-require('module').Module._initPaths();
-
-const Controller = require('lib/controller');
-const Model = require('lib/model');
-const Event = require('lib/event');
+const Controller = require('./lib/controller');
+const Model = require('./lib/model');
+const Event = require('./lib/event');
 
 module.exports = {
   Controller,
