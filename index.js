366b48b3f01f0029d357e5f141eda85e5034e427
Aleksandr Strutynskyy <alekzzz@gmail.com>
2017-05-30 20:34:22 -0700

    Oops wire up service

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const Controller = require('./lib/controller');
 const Model = require('./lib/model');
 const Event = require('./lib/event');
 const Helpers = require('./lib/helpers');
+const Service = require('./lib/service');
 
 module.exports = {
   Controller,
   Model,
   Event,
-  Helpers
+  Helpers,
+  Service
 };
